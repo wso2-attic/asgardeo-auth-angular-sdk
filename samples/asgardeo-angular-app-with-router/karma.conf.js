80f7f48423cc342fe71077927489d53ed4a61b29
JanithGan <janithgan@gmail.com>
2021-02-09 16:38:06 +0530

    Rename sample applications

--- samples/asgardeo-angular-app-with-router/karma.conf.js
+++ samples/asgardeo-angular-app-with-router/karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function (config) {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            dir: require("path").join(__dirname, "./coverage/angular-app-with-router"),
+            dir: require("path").join(__dirname, "./coverage/asgardeo-angular-app-with-router"),
             reports: ["html", "lcovonly", "text-summary"],
             fixWebpackSourcePaths: true
         },
--- samples/asgardeo-angular-app/karma.conf.js
+++ samples/asgardeo-angular-app/karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function (config) {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            dir: require("path").join(__dirname, "./coverage/angular-app"),
+            dir: require("path").join(__dirname, "./coverage/asgardeo-angular-app"),
             reports: ["html", "lcovonly", "text-summary"],
             fixWebpackSourcePaths: true
         },
